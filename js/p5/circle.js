85e73897a048eac0b5a66e1cd9c5e095358522f4
zhao <zhao19323@163.com>
2021-09-02 14:25:10 +0800

    First Commit

---
+++ js/p5/circle.js
@@ -0,0 +1,47 @@
+function Circle( x, y, r,t){
+ var options = {
+  friction: 0.1,
+  restitution: 3,
+  isStatic: true
+}
+
+this.body = Bodies.circle(x,y,r);
+this.r = r;
+World.add(world, this.body);
+
+this.show = function(){
+  var pos = this.body.position;
+  var angle = this.body.angle;
+
+  push();
+  translate(pos.x, pos.y);
+
+  rotate(angle);
+
+  if(windowWidth>450){
+    strokeWeight(2.5);
+  }else{
+    strokeWeight(1.5);
+  }
+
+  fill('#d9d9d9');
+
+  noStroke();
+  ellipse(0,0,this.r*2-2);
+
+  stroke(255,100);
+  strokeWeight(3);
+  noFill();
+  //arc(0, 0, this.r*2*0.85, this.r*2*0.85, -PI/2, -PI/4);
+  //arc(0, 0, this.r*2*0.85, this.r*2*0.85, -PI/5, -PI/7);
+  fill(250);
+  noStroke();
+  textStyle(NORMAL);
+  textSize(this.r/3.5);
+  textAlign(CENTER);
+  fill("#218225");
+  text(t,0,5);
+  pop();
+
+}
+}
